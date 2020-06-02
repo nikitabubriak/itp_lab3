96480836ae1f65ccc313777dcfdf66f44869a087
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2020-06-02 21:17:03 +0300

    Split all the code between function.cpp and function.h, fixed bugs

--- lab3/function.cpp
+++ lab3/function.cpp
@@ -1,5 +1,192 @@
-#include <iostream>
+#include "function.h"
 
 using namespace std;
 
 
+
+void Input_data()
+{
+	ifstream inFile("input.txt");
+	if (!inFile.is_open()) { cout << "Error. Cannot open file\n"; return; }
+
+	inFile >> width >> height;
+	area = width * height;
+
+	cout << "Map:\n\n";
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			char node;
+			inFile >> node;
+			cout << node << " ";
+
+			point[(i * height) + j] = { j + 1, i + 1, node };
+		}
+		cout << "\n";
+	}
+
+	inFile.close();
+}
+
+
+
+void Set_coordinates()
+{
+	//cout << "\nEnter start point coordinates:" << endl;
+	//cout << "\nx= "; cin >> start.x;
+	//cout << "\ny= "; cin >> start.y;
+	start.x = 2; start.y = 7;
+
+	for (int i = 0; i < area; i++)
+	{
+		if (point[i].x == start.x && point[i].y == start.y)
+		{
+			start.type = point[i].type;
+		}
+	}
+
+	//cout << "\nEnter finish point coordinates:" << endl;
+	//cout << "\nx= "; cin >> finish.x;
+	//cout << "\ny= "; cin >> finish.y;
+	finish.x = 7; finish.y = 2;
+
+	for (int i = 0; i < width * height; i++)
+	{
+		if (point[i].x == finish.x && point[i].y == finish.y)
+		{
+			finish.type = point[i].type;
+		}
+	}
+
+	cout << "\nStart>    x= " << start.x << "\ty= " << start.y << endl;
+	cout << "\nFinish>   x= " << finish.x << "\ty= " << finish.y << "\n\n\n Finding path...\n\n" << endl;
+}
+
+
+
+vector<Node> Neighbors(Node current, Node *point, vector<Node> visitedPoint)
+{
+	vector<Node> neighborsOpen;
+
+	for (Node direction : directions)
+	{
+		Node next{ current.x + direction.x, current.y + direction.y };
+
+		for (int i = 0; i < area; i++)
+		{
+			if (next.x == point[i].x && next.y == point[i].y)
+			{
+				next.type = point[i].type;
+			}
+		}
+
+		if (next.type == '.')
+		{
+			for (int i = 0; i < visitedPoint.size(); i++)
+			{
+				if (next.x == visitedPoint[i].x && next.y == visitedPoint[i].y && visitedPoint[i].visited == true)
+				{
+					next.visited = true;
+					break;
+				}
+			}
+
+			if (next.visited == true) { continue; }
+			next.distance_total = INF;
+
+			neighborsOpen.push_back(next);
+		}
+	}
+
+	return neighborsOpen;
+}
+
+
+
+int Heuristics(Node next, Node finish)
+{
+	return abs(next.x - finish.x) + abs(next.y - finish.y);
+}
+
+
+
+vector<Node> AStar(Node *start, Node finish, Node *point)
+{
+	vector<Node> visitedPoint;
+	PriorityQueue queue;
+
+	queue.push(start, 0);
+
+	start->distance_total = 0;
+
+	while (!queue.is_empty())
+	{
+		Node current = queue.pop();
+
+		current.visited = true;
+		visitedPoint.push_back(current);
+
+		if (current.x == finish.x && current.y == finish.y) { cout << "\nReached finish point\n" << endl; break; }
+
+		cout << "\nCurrent>  " << current.x << "\t" << current.y << "\t" << current.type << "\t" << current.visited << "\n" << endl;
+
+		for (Node next : Neighbors(current, point, visitedPoint))
+		{
+			cout << "Neighbor> " << next.x << "\t" << next.y << "\t" << next.type << "\t" << next.visited << endl;
+
+			Node *pnext = &next;
+
+			int distance_new = current.distance_total + 1;
+
+			if (next.distance_total == INF)
+			{
+				pnext->distance_total = distance_new;
+				int npriority = distance_new + Heuristics(next, finish);
+				cout << "---------\n\n";
+
+				queue.push(pnext, npriority);
+			}
+		}
+	}
+
+	return visitedPoint;
+}
+
+
+
+void Draw_path(vector<Node> visited)
+{
+	char path[26];
+
+	for (int i = 0; i < 26; i++)
+	{
+		path[i] = i + 97;
+	}
+
+	for (int k = 0; k < visited.size(); k++)
+	{
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				if (j + 1 == visited[k].x && i + 1 == visited[k].y && visited[k].visited == true)
+				{
+					point[(i * height) + j].type = path[k];
+				}
+			}
+		}
+	}
+
+	cout << "Path:\n\n";
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			cout << point[(i * height) + j].type << " ";
+		}
+		cout << "\n";
+	}
+}
--- lab3/function.h
+++ lab3/function.h
@@ -1,5 +1,140 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const int INF = 999;
+
+int width,
+	height,
+	area;
+
+struct Node
+{
+
+	int x, y;
+	char type;
+
+	int priority;
+
+	int distance_total;
+
+	bool visited;
+
+	Node *q_next;
+};
 
 
 
+class PriorityQueue
+{
+	Node *k;
+
+public:
+
+	PriorityQueue()
+	{
+
+		k = NULL;
+	}
+
+	void push(Node *n, int p)
+	{
+		Node *m;
+		n->priority = p;
+
+		if (k == NULL || p > k->priority)
+		{
+			n->q_next = k;
+			k = n;
+		}
+
+		else
+		{
+			m = k;
+
+			while (m->q_next != NULL && m->q_next->priority >= p)
+			{
+				m = m->q_next;
+			}
+
+			n->q_next = m->q_next;
+			m->q_next = n;
+		}
+	}
+
+	Node pop()
+	{
+		Node *n;
+
+		if (k == NULL)
+		{
+			n = NULL;
+			cout << "\nQueue underflow\n";
+			return *n;
+		}
+
+		else
+		{
+			n = k;
+			k = k->q_next;
+			return *n;
+		}
+	}
+
+	bool is_empty()
+	{
+		Node *ptr;
+		ptr = k;
+
+		if (k == NULL)
+		{
+			cout << "\nQueue is empty\n" << endl;
+			return true;
+		}
+
+		else
+		{
+			return false;
+		}
+	}
+};
+
+
+
+Node *point = new Node[INF];
+
+Node start,
+	finish;
+
+Node *pstart = &start;
+
+Node directions[4] =
+{
+	Node{-1, 0 }, //left
+	Node{ 0, 1 }, //down
+	Node{ 1, 0 }, //right
+	Node{ 0,-1 }  //up
+};
+
+
+
+void Input_data();
+
+void Set_coordinates();
+
+vector<Node> Neighbors(Node, Node*, vector<Node>);
+
+int Heuristics(Node, Node);
+
+vector<Node> AStar(Node*, Node, Node*);
+
+void Draw_path(vector<Node>);
+
+
+
+#include "function.cpp"
